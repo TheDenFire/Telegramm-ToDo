ab38e1b6765a43124313cc5fd6d6592f08acc939
Bogdan <bog040040@yandex.ru>
2022-12-21 06:59:33 +0300

    add library

---
+++ bot.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token='5717622433:AAGxWsBsH1dVCBD3xmyI5c3ON97bj9q-XgU')
+
+dp = Dispatcher()
+
+
+@dp.message(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Иди нахуй пизда тупая")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
